2ee5c97f898e3a8ff3b740328a2d72f0219d274f
Gheorghe Pinzaru <feross95@gmail.com>
2023-11-02 14:42:38 +0100

    Add astro+tailwind template with firebase hosting

---
+++ hosting/src/firebase.ts
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAvXqEHk9Mh7aiUJlExXtxFHp2MvDhi8ww",
+  authDomain: "example-push-3a54c.firebaseapp.com",
+  projectId: "example-push-3a54c",
+  storageBucket: "example-push-3a54c.appspot.com",
+  messagingSenderId: "528678070111",
+  appId: "1:528678070111:web:16520b43917013080378ea"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
